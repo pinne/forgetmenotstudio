00b0a1acfda3614dc4e20ddd7117014b0902a632
Simon Kers <simon@ljusdesign.se>
2021-09-03 09:01:20 +0200

    Changing to lighter font-weight in h1

--- components/layout.js
+++ components/layout.js
@@ -33,7 +33,7 @@ export default function Layout({ children }) {
         <title>FORGET ME NOT STUDIO</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet"></link>
       </Head>
 
         <header>
