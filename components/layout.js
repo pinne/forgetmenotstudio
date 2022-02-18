91e6d1021b45208ebc3085866cf77ad8049cf328
Simon Kers <simon@ljusdesign.se>
2022-02-18 15:45:44 +0100

    Remove js fiddling

--- components/layout.js
+++ components/layout.js
@@ -8,24 +8,6 @@ import debounce from '../util/debounce'
 export default function Layout({ children }) {
   const [height, setHeight] = useState(0)
 
-  useEffect(() => {
-    const updateHeight = debounce(
-      () => setHeight(window.innerHeight),
-      50)
-
-    window.addEventListener('resize', updateHeight)
-    window.addEventListener('orientationchange', updateHeight)
-    window.addEventListener('load', updateHeight)
-    updateHeight()
-
-    return () => {
-      // remove resize listener
-      window.removeEventListener('resize', updateHeight)
-      window.removeEventListener('orientationchange', updateHeight)
-      window.removeEventListener('load', updateHeight)
-    }
-  }, [])
-
   return (
     <div className={styles.container} style={{ minHeight: height }}>
       <TopBar />
@@ -36,11 +18,11 @@ export default function Layout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet"></link>
       </Head>
 
-        <header>
-          <h1>Forget<br />me not<br />studio</h1>
-        </header>
+      <header>
+        <h1>Forget<br />me not<br />studio</h1>
+      </header>
 
-        <main>{children}</main>
+      <main>{children}</main>
 
       <footer>&copy; 2021 Forget me not studio ❀</footer>
     </div>
