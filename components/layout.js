32aed5b2acd74aedabb13b0e9e04d612fd1a8b38
Simon Kers <simon@ljusdesign.se>
2022-02-21 22:06:45 +0100

    Fixing bugs

--- components/layout.js
+++ components/layout.js
@@ -7,22 +7,25 @@ import debounce from '../util/debounce'
 
 export default function Layout({ children }) {
   return (
-    <div className={styles.container}>
-      <TopBar />
-      <Head>
-        <title>FORGET ME NOT STUDIO</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet"></link>
-      </Head>
+    <>
+      <div className="bg"></div>
+      <div className={styles.container}>
+        <TopBar />
+        <Head>
+          <title>FORGET ME NOT STUDIO</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet"></link>
+        </Head>
 
-      <header>
-        <h1>Forget<br />me not<br />studio</h1>
-      </header>
+        <header>
+          <h1>Forget<br />me not<br />studio</h1>
+        </header>
 
-      <main>{children}</main>
+        <main>{children}</main>
 
-      <footer>&copy; 2021 Forget me not studio ❀</footer>
-    </div>
+        <footer>&copy; 2021 Forget me not studio ❀</footer>
+      </div>
+    </>
   )
 }
