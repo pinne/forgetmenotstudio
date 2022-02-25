768711a87c7b740f5d35aa7d4ef9ec5a671098e0
Simon Kers <simon@ljusdesign.se>
2022-02-25 14:15:33 +0100

    Fixing menu for mobile and desktop

--- components/topbar.js
+++ components/topbar.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 import styles from './topbar.module.css'
+import classnames from 'classnames'
 
 function TopBar() {
   return (
     <div className={styles.container}>
-      <div className={styles.forgetMeNot}>
+      <div className={classnames(styles.forgetMeNot, styles.desktop)}>
         <Link href="/">
           <a>Forget me not studio</a>
+        </Link>
+      </div>
+      <div className={classnames(styles.forgetMeNot, styles.mobile)}>
+        <Link href="/">
+          <a>Home</a>
         </Link>
       </div>
       <ul className={styles.nav}>
