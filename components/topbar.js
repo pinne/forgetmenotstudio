6ef30266d1f6e1f8fcbb08b046c590138255ea3a
Simon Kers <simon@ljusdesign.se>
2022-02-25 13:56:24 +0100

    Adding hamburger menu

--- components/topbar.js
+++ components/topbar.js
@@ -1,26 +1,37 @@
 import Link from 'next/link'
+import { Rotate as Hamburger } from 'hamburger-react'
+import { useState } from 'react'
 import styles from './topbar.module.css'
 
+function Modal() {
+  return (
+    <ul className={styles.nav}>
+      <li className={styles.link}>
+        <Link href="/stories">
+          <a>Stories</a>
+        </Link>
+      </li>
+      <li className={styles.link}>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+      </li>
+    </ul>
+  )
+}
+
 function TopBar() {
+  const [isOpen, setOpen] = useState(false)
+
   return (
     <div className={styles.container}>
+      {isOpen ? <Modal /> : null}
       <div className={styles.forgetMeNot}>
         <Link href="/">
           <a>Forget me not studio</a>
         </Link>
       </div>
-      <ul className={styles.nav}>
-        <li className={styles.link}>
-          <Link href="/stories">
-            <a>Stories</a>
-          </Link>
-        </li>
-        <li className={styles.link}>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </li>
-      </ul>
+      <Hamburger color={'rgb(233, 170, 235)'} toggled={isOpen} toggle={setOpen} label="Show menu" size={48} />
     </div>
   )
 }
