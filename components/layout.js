af8aa9960db79a4b8e443237299cd97fa2c6ec09
Simon Kers <simon@ljusdesign.se>
2022-09-15 14:16:28 +0200

    New footer design

--- components/layout.js
+++ components/layout.js
@@ -15,11 +15,31 @@ export default function Layout({ children }) {
 
         {children}
 
-        <footer id="#footer">
-          <p>&copy; 2021-2022 Forget me not Studio</p>
-          <p>FMN Studio AB</p>
-          <p>Stockholm, Sweden</p>
-          <p>Org # 559378-6105</p>
+        <footer>
+          <div className={styles.companyInfo}>
+            <p>FMN Studio AB</p>
+            <p>Stockholm, Sweden</p>
+            <p>Org # 559378-6105</p>
+          </div>
+          <div className={styles.copyright}>
+            <SocialIcon
+              url="https://instagram.com/_forgetmenotstudio_"
+              height="25"
+              width="25"
+              bgColor="rgb(220, 225, 241)"
+              fgColor="#171924"
+              target="_blank"
+            />
+            &nbsp;
+            <SocialIcon
+              url="mailto:hello@forgetmenotstudio.se"
+              height="25"
+              width="25"
+              bgColor="rgb(220, 225, 241)"
+              fgColor="#171924"
+            />
+            <p>&copy; 2021-2022 Forget me not Studio</p>
+          </div>
         </footer>
       </div>
     </>
