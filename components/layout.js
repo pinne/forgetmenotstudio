ed0d845fb0d49b0ae400e2b2492a4a19490eef73
Simon Kers <simon@ljusdesign.se>
2022-09-15 14:24:34 +0200

    Adjusting pixels in footer

--- components/layout.js
+++ components/layout.js
@@ -23,22 +23,24 @@ export default function Layout({ children }) {
             <p>Org # 559378-6105</p>
           </div>
           <div className={styles.copyright}>
-            <SocialIcon
-              url="https://instagram.com/_forgetmenotstudio_"
-              height="25"
-              width="25"
-              bgColor="rgb(220, 225, 241)"
-              fgColor="#171924"
-              target="_blank"
-            />
-            &nbsp;
-            <SocialIcon
-              url="mailto:hello@forgetmenotstudio.se"
-              height="25"
-              width="25"
-              bgColor="rgb(220, 225, 241)"
-              fgColor="#171924"
-            />
+            <div>
+              <SocialIcon
+                url="https://instagram.com/_forgetmenotstudio_"
+                height="25"
+                width="25"
+                bgColor="rgb(220, 225, 241)"
+                fgColor="#171924"
+                target="_blank"
+              />
+              &nbsp;
+              <SocialIcon
+                url="mailto:hello@forgetmenotstudio.se"
+                height="25"
+                width="25"
+                bgColor="rgb(220, 225, 241)"
+                fgColor="#171924"
+              />
+            </div>
             <p>&copy; 2021-2022 Forget me not Studio</p>
           </div>
         </footer>
