036786814680c16e8be7733a10676e9a7e2fe61f
Simon Kers <simon@ljusdesign.se>
2022-09-15 20:42:45 +0200

    Adding project puff to stories page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import Layout from '../components/layout'
 import Contacts from '../components/contacts'
-import ProjectPuff from '../components/project-puff'
 
 export default function Home() {
   return (
