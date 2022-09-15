2f6c3a7f29c09839a29663b6d497524636fdb19d
Simon Kers <simon@ljusdesign.se>
2022-09-15 20:42:45 +0200

    Adding project puff to stories page

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -1,5 +1,40 @@
+import Image from 'next/image'
+import aptStyling from '../../images/apt-styling.jpg'
+import tavla from '../../images/bild-med-tavla.png'
+import styles from '../../components/project-puff.module.css'
+
+function LargePuff({image, project, description}) {
+  return (
+    <div className={styles.puff}>
+      <a href="#">
+        <Image
+          src={image}
+          className={styles.cover}
+          width="350"
+          height="350"
+        />
+        <div className={styles.bottom}>
+          <div>{project}</div>
+          <div>{description}</div>
+        </div>
+      </a>
+    </div>
+  )
+}
 export default function Stories ({ stories }) {
   return <div>
     <h1>Stories</h1>
+    <div className={styles.container}>
+      <LargePuff
+        image={tavla}
+        project="Home styling"
+        description="Residential concept"
+      /> 
+      <LargePuff
+        image={aptStyling}
+        project="Living room"
+        description="Residential concept"
+      /> 
+      </div>
   </div>
 }
